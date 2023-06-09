9d8ddbf38e8dc49462215cd5b391bda1e1b5c0e4
zrierc <68382685+zrierc@users.noreply.github.com>
2023-06-09 15:44:59 +0700

    feat: add query string support

--- index.ts
+++ index.ts
@@ -1,4 +1,25 @@
-import { CloudFrontFunctionsEvent } from 'aws-lambda';
+import {
+  CloudFrontFunctionsEvent,
+  CloudFrontFunctionsQuerystring,
+} from 'aws-lambda';
+
+function getURLSearchParamsString(querystring: CloudFrontFunctionsQuerystring) {
+  let str = [];
+  for (const param in querystring) {
+    const query = querystring[param];
+    const multiValue = query.multiValue;
+
+    if (multiValue) {
+      str.push(multiValue.map(item => param + '=' + item.value).join('&'));
+    } else if (query.value === '') {
+      str.push(param);
+    } else {
+      str.push(param + '=' + query.value);
+    }
+  }
+
+  return str.join('&');
+}
 
 function handler(event: CloudFrontFunctionsEvent) {
   const request = event.request;
@@ -9,11 +30,15 @@ function handler(event: CloudFrontFunctionsEvent) {
   if (headers['host']) {
     const host = headers['host'].value;
     const fullDomainName = host.split('.');
-    let newUrl = '';
     const baseUrl = fullDomainName.slice(fullDomainName.length - 2).join('.');
+    let newUrl = '';
 
     if (fullDomainName.length === 3 && fullDomainName[0] !== 'www') {
-      newUrl = 'https://' + baseUrl + '/' + fullDomainName[0] + uri;
+      if (Object.keys(request['querystring']).length === 0) {
+        newUrl = 'https://' + baseUrl + '/' + fullDomainName[0] + uri;
+      } else {
+        newUrl = 'https://' + baseUrl + '/' + fullDomainName[0] + uri + '?' + getURLSearchParamsString(request.querystring);
+      }
 
       const response = {
         statusCode: 301,
