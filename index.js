dfa90c67b3004f5fee2750974a3bbc7162e5d796
zrierc <68382685+zrierc@users.noreply.github.com>
2023-06-09 19:12:03 +0700

    feat: ready to use

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-import {
-  CloudFrontFunctionsEvent,
-  CloudFrontFunctionsQuerystring,
-} from 'aws-lambda';
-
-function getURLSearchParamsString(querystring: CloudFrontFunctionsQuerystring) {
-  let str = [];
-  for (const param in querystring) {
-    const query = querystring[param];
-    const multiValue = query.multiValue;
+function getURLSearchParamsString(querystring) {
+  var str = [];
+  for (var param in querystring) {
+    var query = querystring[param];
+    var multiValue = query.multiValue;
 
     if (multiValue) {
       str.push(multiValue.map(item => param + '=' + item.value).join('&'));
@@ -21,17 +16,17 @@ function getURLSearchParamsString(querystring: CloudFrontFunctionsQuerystring) {
   return str.join('&');
 }
 
-function handler(event: CloudFrontFunctionsEvent) {
-  const request = event.request;
-  const headers = request.headers;
-  let uri = '';
+function handler(event) {
+  var request = event.request;
+  var headers = request.headers;
+  var uri = '';
   if (request.uri) uri = request.uri;
 
   if (headers['host']) {
-    const host = headers['host'].value;
-    const fullDomainName = host.split('.');
-    const baseUrl = fullDomainName.slice(fullDomainName.length - 2).join('.');
-    let newUrl = '';
+    var host = headers['host'].value;
+    var fullDomainName = host.split('.');
+    var newUrl = '';
+    var baseUrl = 'www.' + fullDomainName.slice(fullDomainName.length - 2).join('.');
 
     if (fullDomainName.length === 3 && fullDomainName[0] !== 'www') {
       if (Object.keys(request['querystring']).length === 0) {
@@ -40,7 +35,7 @@ function handler(event: CloudFrontFunctionsEvent) {
         newUrl = 'https://' + baseUrl + '/' + fullDomainName[0] + uri + '?' + getURLSearchParamsString(request.querystring);
       }
 
-      const response = {
+      var response = {
         statusCode: 301,
         statusDescription: 'Moved Permanently',
         headers: { location: { value: newUrl } },
